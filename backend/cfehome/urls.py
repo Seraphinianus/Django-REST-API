535f75ec21f53f42241035a1f083d8de244515c4
Sebastian Dima <sebastian.dima@stud.ubbcluj.ro>
2022-05-04 17:31:02 +0300

    Finished requirements.txt, testing the endpoint on localhost and adding the 2 folder structures

--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -1,4 +1,4 @@
-"""Django_REST_API URL Configuration
+"""cfehome URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
